3a195d76bc508bc407420d0266bff58d4960a278
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2019-08-29 16:21:42 +0900

    使ってないファイル削除

--- 03_python_environmet_setup.sh
+++
@@ -1,8 +0,0 @@
-# Python tools
-sudo apt -y install python-dev
-sudo apt -y install python-serial
-sudo apt -y install python-testresources
-# pip latest
-sudo apt -y purge python-pip
-wget https://bootstrap.pypa.io/get-pip.py
-sudo python get-pip.py
