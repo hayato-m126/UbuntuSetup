ce412d48a9e9f92abf6041d7f57c47e37f222c28
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2019-08-29 16:24:56 +0900

    XRDPは繋がらなくなってたので削除

--- 12_remote_control_and_fileshare.sh
+++ 12_remote_control_and_fileshare.sh
@@ -3,32 +3,6 @@ UBUNTU_VER=$(lsb_release -sc)
 # install ssh-server
 sudo apt -y install openssh-server
 
-# RDP
-if [ "$UBUNTU_VER" = "xenial" ]; then
-    sudo add-apt-repository -y ppa:hermlnx/xrdp
-    sudo apt update
-fi
-sudo apt -y install xrdp
-
-# RDP設定、new_cursorsの無効化
-sudo sed -e 's/^new_cursors=true/new_cursors=false/g' \
- -i /etc/xrdp/xrdp.ini
-
-# xrdpサービスの再起動
-sudo systemctl restart xrdp
-
-# xsessionファイルの作成、cinnamonでログインする
-# Ubuntu18.04では本体側でログアウトが必要。ログイン状態だとリモート出来ない。
-echo "cinnamon-session" > ~/.xsession
-D=/usr/share/gnome:/usr/share/cinnamon:/usr/local/share:/usr/share
-D=${D}:/var/lib/snapd/desktop
-C=/etc/xdg/xdg-cinnamon:/etc/xdg
-cat <<EOF > ~/.xsessionrc
-export XDG_SESSION_DESKTOP=cinnamon
-export XDG_DATA_DIRS=${D}
-export XDG_CONFIG_DIRS=${C}
-EOF
-
 # windows file share
 sudo apt -y install samba
 # share ~/share
@@ -39,5 +13,3 @@ echo "read only = No" | sudo tee -a /etc/samba/smb.conf
 echo "guest ok = Yes" | sudo tee -a /etc/samba/smb.conf
 echo "force user = $USER" | sudo tee -a /etc/samba/smb.conf
 sudo service smbd restart
-
-
