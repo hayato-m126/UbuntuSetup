7b919b6657a400c4a1d6b0d6eb1be88a6116a97b
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2019-08-29 16:25:47 +0900

    docker-composeをaptからインストールするように変更。セキュリティの観点からユーザーをdockerグループに追加するのはやめる

--- 08_docker_install.sh
+++ 08_docker_install.sh
@@ -6,12 +6,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable"
 sudo apt -y update
 sudo apt -y install docker-ce docker-ce-cli containerd.io
-# add user to docker group
-sudo gpasswd -a $USER docker
-# install docker-compose
-# check github latest release and replace version number.
-sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
 
-# グループに追加しても、dockerコマンドをユーザーのまま実行出来なかった。再起動したらいけた
-sudo reboot
+# install docker-compose
+sudo apt -y install docker-compose
