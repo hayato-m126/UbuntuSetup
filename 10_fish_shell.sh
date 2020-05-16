e3d7afa5abd96697636eedb3615f8c79431a4010
Hayato Mizushima <hayato.mizushima@tier4.jp>
2020-05-16 14:07:08 +0900

    feat: add fish shell

---
+++ 10_fish_shell.sh
@@ -0,0 +1,15 @@
+sudo apt-add-repository -y ppa:fish-shell/release-3
+sudo apt update
+sudo apt -y install fish
+
+curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+# fisher add oh-my-fish/theme-yimmy
+# fisher add edc/bass
+# fisher add jethrokuan/z
+# fisher add jethrokuan/fzf
+
+
