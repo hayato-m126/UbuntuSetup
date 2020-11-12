f53dd476049b730a3b304f0d4404f4cc5f6af5cf
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2020-11-12 17:26:31 +0900

    feat: install driver

--- 00_initial_update_and_settings.sh
+++ 00_initial_update_and_settings.sh
@@ -52,6 +52,9 @@ sudo apt -y install google-chrome-stable
 #なぜかキーが重複していると言われるので消す
 sudo rm /etc/apt/sources.list.d/google.list
 
+# install driver, nvidia driver
+sudo ubuntu-drivers autoinstall
+
 # install Hardware Enablement kernel
 if [ "$UBUNTU_VER" = "xenial" ]; then
     sudo apt -y install --install-recommends linux-generic-hwe-16.04
--- option/nvidia_docker.sh
+++ option/nvidia_docker.sh
@@ -1,15 +1,5 @@
 UBUNTU_VER=$(lsb_release -sc)
 
-# install Graphic Driver
-sudo add-apt-repository ppa:graphics-drivers/ppa
-if [ "$UBUNTU_VER" = "xenial" ]; then
-    sudo apt -y update
-    sudo apt -y install nvidia-430
-fi
-if [ "$UBUNTU_VER" = "bionic" ]; then
-    sudo apt -y install nvidia-driver-450
-fi
-
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
