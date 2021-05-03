be042be055a053e200f54839833dd3e020fa79ff
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2021-05-03 16:19:47 +0900

    feat: pyenv build dependencies

--- 04_dev_tool.sh
+++ 04_dev_tool.sh
@@ -4,6 +4,11 @@ git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0
 mkdir -p ~/.config/fish/completions
 ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
 
+# for pyenv build
+sudo apt-get install -y build-essential libssl-dev zlib1g-dev libbz2-dev \
+libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+xz-utils tk-dev libffi-dev liblzma-dev python-openssl
+
 # python tool
 python3 -m pip install --user pipx
 python3 -m pipx ensurepath
