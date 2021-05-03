179993d691a983103ca46027f96700a2f0152bdd
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2021-05-03 16:53:25 +0900

    feat: update dev tool

--- 04_dev_tool.sh
+++ 04_dev_tool.sh
@@ -14,7 +14,9 @@ python3 -m pip install --user pipx
 python3 -m pipx ensurepath
 pipx completions
 
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py | python -
+pipx install argcomplete
+
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py | python3 -
 
 # make tool
 sudo apt -y install cmake cmake-curses-gui
