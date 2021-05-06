7a2050a4633591ccf2297e1d32649fe6751a842d
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2021-05-06 18:31:18 +0900

    fix: to execute

--- shell/option/nvidia_docker.sh
+++ shell/option/nvidia_docker.sh
@@ -1,18 +1,6 @@
-UBUNTU_VER=$(lsb_release -sc)
-
-# install Graphic Driver
-sudo add-apt-repository ppa:graphics-drivers/ppa
-if [ "$UBUNTU_VER" = "xenial" ]; then
-    sudo apt -y update
-    sudo apt -y install nvidia-430
-fi
-if [ "$UBUNTU_VER" = "bionic" ]; then
-    sudo apt -y install nvidia-driver-450
-fi
 if [ "$UBUNTU_VER" = "focal" ]; then
     sudo apt -y install nvidia-driver-455
 fi
-
 
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
--- shell/option/ssh_setting.sh
+++ shell/option/ssh_setting.sh
@@ -1,13 +1,3 @@
-if [ $# -ne 2 ]; then
-  echo "usage: ./ssh_settings.sh public_key port_number"
-  exit 1
-fi
-
-mkdir -p ~/.ssh
-chmod 700 ~/.ssh
-echo $1 > ~/.ssh/authorized_keys
-chmod 600 ~/.ssh/authorized_keys
-
 # ssh security setting
 sudo sed -i -e s/"#Port 22"/"Port $2"/ /etc/ssh/sshd_config
 sudo sed -i -e s/"#AuthorizedKeysFile"/"AuthorizedKeysFile"/ /etc/ssh/sshd_config
