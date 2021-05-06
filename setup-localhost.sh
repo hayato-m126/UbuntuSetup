133fe201bbed12f95be23b34bca8b10fde7422aa
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2021-05-06 21:21:48 +0900

    feat: update

--- setup-localhost.sh
+++ setup-localhost.sh
@@ -7,4 +7,4 @@ if !(command -v ansible-playbook  > /dev/null 2>&1); then
     sudo apt install -y ansible
 fi
 
-ansible-playbook -i localhost -c local setup-ubuntu20.04.yml --ask-become-pass
+ansible-playbook -i localhost -c local setup-ubuntu20.04.yml --ask-become-pass -e ansible_user=$USER
--- shell/option/ssh_setting.sh
+++
@@ -1,6 +0,0 @@
-# ssh security setting
-sudo sed -i -e s/"#Port 22"/"Port $2"/ /etc/ssh/sshd_config
-sudo sed -i -e s/"#AuthorizedKeysFile"/"AuthorizedKeysFile"/ /etc/ssh/sshd_config
-sudo sed -i -e s/"#PasswordAuthentication yes"/"PasswordAuthentication no"/ /etc/ssh/sshd_config
-
-sudo sed -i -e s/"PermitRootLogin prohibit-password"/"PermitRootLogin no"/ /etc/ssh/sshd_config
