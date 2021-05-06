5b382a8993887d3d7058dc1c718f3d7c36349d06
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2021-05-06 21:25:08 +0900

    Feat/use ansible (#2)
    * feat: ansible template
    
    * feat: delete unused shell scirpt
    
    * feat: update system tools
    
    * feat: update ide and desktop env
    
    * feat: update dev tool
    
    * feat: asciidoc
    
    * feat: add become
    
    * feat: update system
    
    * fix: add python3-pip
    
    * feat: comment out tasks not working
    
    * feat: install docker and multimedia
    
    * update: multimedia
    
    * feat: add binary install
    
    * fix: to execute
    
    * fix: commnet out pipx
    
    * fix: lineinfile to replace
    
    * fix: add pipx dependency
    
    * feat: add user to group
    
    * feat: update

--- 08_fish_shell.sh
+++
@@ -1,6 +0,0 @@
-if [ $(lsb_release -sc) != "focal" ]; then
-    sudo apt-add-repository -y ppa:fish-shell/release-3
-    sudo apt update
-fi
-sudo apt -y install fish
-curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
---
+++ setup-localhost.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt -y update
+sudo apt -y upgrade
+
+if !(command -v ansible-playbook  > /dev/null 2>&1); then
+    sudo apt install -y ansible
+fi
+
+ansible-playbook -i localhost -c local setup-ubuntu20.04.yml --ask-become-pass -e ansible_user=$USER
