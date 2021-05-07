0b1f07fc6e5ff8097207ed32c180a331e7f48489
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2021-05-07 12:15:16 +0900

    feat: add local setup

--- setup-localhost.sh
+++ setup-localhost.sh
@@ -7,4 +7,4 @@ if !(command -v ansible-playbook  > /dev/null 2>&1); then
     sudo apt install -y ansible
 fi
 
-ansible-playbook -i localhost -c local ansible/setup-ubuntu20.04.yml --ask-become-pass -e ansible_user=$USER
+ansible-playbook ansible/setup-ubuntu20.04.yml --ask-become-pass -e ansible_user=$USER
