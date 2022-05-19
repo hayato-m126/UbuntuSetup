05758f4b10380d582bc287388d29bc267d80ddbd
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2022-05-19 13:54:54 +0900

    feat: use ansible5 and enable install pyenv depend

--- setup-localhost.sh
+++ setup-localhost.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-if !(command -v ansible-playbook  > /dev/null 2>&1); then
-    sudo apt -y update
-    sudo apt -y upgrade
-    sudo apt install -y ansible
+if ! (command -v pip3 >/dev/null 2>&1); then
+    sudo apt-get -y update
+    sudo apt-get -y install python3-pip
+fi
+
+# Install ansible
+ansible_version=$(pip3 list | grep -oP "^ansible\s+\K([0-9]+)" || true)
+if [ "$ansible_version" != "5" ]; then
+    sudo apt-get -y purge ansible
+    pip3 install -U "ansible==5.*"
 fi
 
 ansible-playbook -i ansible/inventories/localhost.ini ansible/setup-ubuntu22.04.yml --ask-become-pass -e ansible_user=$USER
