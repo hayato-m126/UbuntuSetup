589fbb52afc54f66665766f42b9c8d629bb3cea4
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2024-04-19 02:27:45 +0900

    feat: update setup script

--- setup-localhost.sh
+++ setup-localhost.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
 if ! (command -v curl >/dev/null 2>&1); then
-    sudo apt install curl
+    sudo apt -y update
+    sudo apt -y install curl
 fi
 if ! (command -v rye >/dev/null 2>&1); then
     # https://rye-up.com/guide/installation/#customized-installation
     curl -sSf https://rye-up.com/get | RYE_INSTALL_OPTION="--yes" bash
 fi
+source $HOME/.rye/env
 if ! (command -v ansible-playbook >/dev/null 2>&1); then
-    source $HOME/.rye/env
+    # to avoid install ansible directory
+    # ERROR: ansible@ file:///app/UbuntuSetup/ansible from file:///app/UbuntuSetup/ansible does not appear to be a Python project: neither 'setup.py' nor 'pyproject.toml' found.
+    cd tips
+    # ansible directory to be renamed.
     rye install ansible
-    export PATH=$HOME/.rye/tools/ansible/bin:$PATH
+    cd ../
 fi
+export PATH=$HOME/.rye/tools/ansible/bin:$PATH
+# dockerの場合とか考えると、パスワード聞かれるの都合悪い。TODO
 ansible-playbook -i ansible/inventories/localhost.ini ansible/setup-ubuntu22.04.yml --ask-become-pass -e ansible_user=$USER -t dotfiles
