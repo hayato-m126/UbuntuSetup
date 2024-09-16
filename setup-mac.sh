1bf6d772723f460f227768b86f314ad7ef3f59e9
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2024-09-16 18:12:24 +0900

    feat: add brew path
    Signed-off-by: Hayato Mizushima <hayato-m126@users.noreply.github.com>

--- setup-mac.sh
+++ setup-mac.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/bin/zsh
 
 if ! (command -v brew >/dev/null 2>&1); then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
-if ! (command -v rye >/dev/null 2>&1); then
-    # https://rye-up.com/guide/installation/#customized-installation
-    curl -sSf https://rye.astral.sh/get | RYE_INSTALL_OPTION="--yes" bash
-fi
+# if ! (command -v rye >/dev/null 2>&1); then
+#     # https://rye-up.com/guide/installation/#customized-installation
+#     curl -sSf https://rye.astral.sh/get | RYE_INSTALL_OPTION="--yes" bash
+# fi
 source $HOME/.rye/env
 if ! (command -v ansible-playbook >/dev/null 2>&1); then
     rye install ansible-core
